597805dc1b34afaf86a68151ce33653d28e97fd2
eliosoFH <wi23b100@technikum-wien.at>
2025-05-07 15:32:13 +0200

    TEST

--- EnergyJavaFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.demjavafxbb {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.demjavafxbb to javafx.fxml;
-    exports org.example.demjavafxbb;
-}
